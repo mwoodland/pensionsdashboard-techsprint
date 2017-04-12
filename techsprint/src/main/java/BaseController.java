34bc450eaacdc022ca106db8a37b3263d0778676
mwoodland <mark.woodland1@googlemail.com>
2017-04-12 13:37:37 +0100

    Initial Spring boot app

---
+++ techsprint/src/main/java/BaseController.java
@@ -0,0 +1,24 @@
+/**
+ * Created by mwoodland on 11/04/2017.
+ */
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class BaseController {
+    
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(BaseController.class, args);
+    }
+    
+}
